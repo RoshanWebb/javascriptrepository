bb6023f888bbfde0e6663105b946520fec8604e0
RoshanWebb <51826707+RoshanWebb@users.noreply.github.com>
2019-06-14 19:11:51 +0200

    Add files via upload

---
+++ Chapter 11/chapter11.js
@@ -0,0 +1,18 @@
+var people = ["Mary", "Bobby", "Judy", "Eddie", "Herbie", "Tony", "Bobby"];
+var otherPeople = ["Teddy", "Cathy", "G-berg", "Georgie"];
+
+//people = people.concat(otherPeople);
+//people = people.indexOf("Eddie");
+//people = people.join(" # ");
+//people = people.lastIndexOf("Bobby");
+//people.pop();
+//people.push("Teddy");
+//people = people.reverse();
+//people.shift();
+//people.unshift("Teddy");
+//people = people.slice(0,3);
+//people = people.sort();
+//people.splice(1,0,"Cathy");
+
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
